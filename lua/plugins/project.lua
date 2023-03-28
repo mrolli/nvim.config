5905a8dd351a9f493913678094a8588638e9ae76
Michael Rolli <michael@rollis.ch>
2023-03-28 06:04:47 +0200

    Add telescope extension projects
    Adding a button "Recent projects" to the dashboard does not work as
    expected. Needs more insights on how to achieve that.

---
+++ lua/plugins/project.lua
@@ -0,0 +1,18 @@
+-- Setup project plugin
+-- see https://github.com/ahmedkhalf/project.nvim
+
+return {
+  {
+    "ahmedkhalf/project.nvim",
+    enabled = true,
+    lazy = false,
+    config = function()
+      require("project_nvim").setup({
+        detection_methods = { "pattern" },
+      })
+      require("telescope").load_extension("projects")
+      -- local dashboard = require("alpha.themes.dashboard")
+      -- dashboard.button("g", " " .. " Recent projects", ":Telescope projects <CR>")
+    end,
+  },
+}
