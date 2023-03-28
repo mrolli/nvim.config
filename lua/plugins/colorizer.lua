1db15ff080510588e8753c1fb2c5e9bd4b66c9f0
Michael Rolli <michael@rollis.ch>
2023-03-28 06:06:33 +0200

    Fix structure of plugin config

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -2,22 +2,24 @@
 -- see https://github.com/norcalli/nvim-colorizer.lua
 --
 return {
-  "norcalli/nvim-colorizer.lua",
-  enabled = true,
-  config = function()
-    require("colorizer").setup({
-      "*",
-      "!gitcommit",
-    }, {
-      RGB = true, -- #RGB hex codes
-      RRGGBB = true, -- #RRGGBB hex codes
-      RRGGBBAA = true, -- #RRGGBBAA hex codes
-      rgb_fn = true, -- CSS rgb() and rgba() functions
-      hsl_fn = true, -- CSS hsl() and hsla() functions
-      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      -- Available modes: foreground, background
-      mode = "background", -- Set the display mode.
-    })
-  end,
+  {
+    "norcalli/nvim-colorizer.lua",
+    enabled = true,
+    config = function()
+      require("colorizer").setup({
+        "*",
+        "!gitcommit",
+      }, {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        -- Available modes: foreground, background
+        mode = "background", -- Set the display mode.
+      })
+    end,
+  },
 }
