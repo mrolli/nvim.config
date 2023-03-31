ceca78aeca4997d40a13fb8151729f00e6132f4e
Michael Rolli <michael@rollis.ch>
2023-03-31 22:54:32 +0200

    Disable direct clipboard and add keymaps
    I don't like the automatic yanking/pasting to/from the system clipboard.
    Therefore disable this and some custom keymaps using the leader key to
    explicitly yank/paste to/from the system clipboard.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,34 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+--[[
+  Using leader + y/Y to yank to unnameplus or paste from unnamedplus register.
+  The register unnamedplus is the system's clipboard and is the default
+  clipboard register in lvim, but I don't like that.
+  Therefore clipboard is changed to unnamed (see lua/user/neovim.lua) and these
+  additional mappings using the leader key are in place to yank to and paste
+  from the unnamedplus register.
+  Found at https://github.com/LunarVim/LunarVim/issues/930#issuecomment-890597260
+  ]]
+
+-- Yank to system clipboard
+map("n", "<leader>y", '"+y', { desc = "Yank to clipboard" })
+map("n", "<leader>yy", '"+yy', { desc = "Yank line to clipboard" })
+map("v", "<leader>y", '"+y', { desc = "Yank to clipboard" })
+
+-- Paste from system clipbard
+map("n", "<leader>p", '"+p', { desc = "Paste from clipboard" })
+map("n", "<leader>P", '"+P', { desc = "Paste from clipboard" })
+map("v", "<leader>p", '"+p', { desc = "Paste from clipboard" })
