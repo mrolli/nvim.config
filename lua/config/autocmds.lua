9dbbcbea139eed4f9b4d05c3d72b484bd0a634cd
Michael Rolli <michael@rollis.ch>
2023-03-31 22:17:30 +0200

    Automatically set text width for markdown files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Automatically set text width for markdown files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "markdown" },
+  callback = function()
+    vim.opt.tw = 80
+  end,
+})
