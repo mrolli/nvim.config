b6f609e45cbc8bd2d87554351fa6035bef2679fc
Michael Rolli <michael@rollis.ch>
2023-04-01 12:19:42 +0200

    Add two example autocmds for illustration purposes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,19 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = { "*.json", "*.jsonc" },
+--   -- enable wrap mode for json files only
+--   command = "setlocal wrap",
+-- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
+
 -- Automatically set text width for markdown files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "markdown" },
