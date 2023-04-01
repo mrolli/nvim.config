2acaa225093aea16d9a70e90ebf83759e01671e6
Michael Rolli <michael@rollis.ch>
2023-04-01 14:32:16 +0200

    Reconfigure wildmenu to my taste

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,16 @@ vim.opt.colorcolumn = "80"
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 7
+
+-- configure wildmenu related options
+vim.opt.wildmode = "full"
+vim.opt.wildoptions = "pum"
+vim.opt.wildignore = {
+  "*~",
+  "**/.git/*",
+  "**/.vagrant/*",
+  "*.o",
+  "*.pyc",
+  "*pycache*",
+}
+vim.opt.pumblend = 10 -- make pop-up menu slightly transparent
