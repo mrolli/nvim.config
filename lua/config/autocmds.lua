a12fb49cf87351770f588fc8e1f1c0d7e145d416
Michael Rolli <michael@rollis.ch>
2023-04-05 10:37:35 +0200

    Add link to documentation

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+-- See https://neovim.io/doc/user/autocmd.html
 
 -- vim.api.nvim_create_autocmd("BufEnter", {
 --   pattern = { "*.json", "*.jsonc" },
