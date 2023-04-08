ea730c1a567c98ea8508304a6f8eaefaa6f7dd9b
Michael Rolli <michael@rollis.ch>
2023-04-08 07:16:18 +0200

    Fix dict struct returned here

--- lua/plugins/template.lua
+++ lua/plugins/template.lua
@@ -2,14 +2,17 @@
 -- see https://github.com/<OWNER>/<PLUGIN>.nvim
 
 -- remove these two lines upon copying this template
-if true then return {} end
+if true then
+  return {}
+end
 
 return {
-  "<OWNER>/<PLUGIN>",
-  enabled = true,
-  opts = {
-    -- add your options that should be passed to the setup() function here
-    --option = "value",
-
-    }
+  {
+    "<OWNER>/<PLUGIN>",
+    enabled = true,
+    opts = {
+      -- add your options that should be passed to the setup() function here
+      --option = "value",
+    },
+  },
 }
