f1fb468e82dcba18ad36c12ad63433afe8ca6803
Michael Rolli <michael@rollis.ch>
2023-04-10 11:45:28 +0200

    Show warning on mixed indenting

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -17,6 +17,30 @@ local conditions = {
   end,
 }
 
+local function mixed_indenting()
+  local space_pat = [[\v^ +]]
+  local tab_pat = [[\v^\t+]]
+  local space_indent = vim.fn.search(space_pat, 'nwc')
+  local tab_indent = vim.fn.search(tab_pat, 'nwc')
+  local mixed = (space_indent > 0 and tab_indent > 0)
+  local mixed_same_line
+  if not mixed then
+    mixed_same_line = vim.fn.search([[\v^(\t+ | +\t)]], 'nwc')
+    mixed = mixed_same_line > 0
+  end
+  if not mixed then return '' end
+  if mixed_same_line ~= nil and mixed_same_line > 0 then
+     return 'MI:'..mixed_same_line
+  end
+  local space_indent_cnt = vim.fn.searchcount({pattern=space_pat, max_count=1e3}).total
+  local tab_indent_cnt =  vim.fn.searchcount({pattern=tab_pat, max_count=1e3}).total
+  if space_indent_cnt > tab_indent_cnt then
+    return 'MI:'..tab_indent
+  else
+    return 'MI:'..space_indent
+  end
+end
+
 -- See https://github.com/nvim-lualine/lualine.nvim#general-component-options
 local components = {
   encoding = {
@@ -37,6 +61,11 @@ local components = {
   },
   filetype = { "filetype", cond = nil, padding = 0 },
   location = { "location", cond = nil, padding = 0 },
+  mixedindent = {
+    mixed_indenting(),
+    cond = nil,
+    padding = 0,
+  },
   progress = { "progress", cond = nil, padding = 0 },
   spaces = {
     function()
@@ -70,6 +99,7 @@ return {
           components.fileformat,
           components.encoding,
           components.spaces,
+          components.mixedindent,
         },
         lualine_z = {
           components.progress,
