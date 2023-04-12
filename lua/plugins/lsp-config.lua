f32f0c52c646554cd7a4ba56958e885f484d2aa3
Michael Rolli <michael@rollis.ch>
2023-04-12 06:36:19 +0200

    (lsp) Activate more null-ls builtins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -45,4 +45,23 @@ return {
       })
     end,
   },
+
+  -- null-ls
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      vim.list_extend(opts.sources, {
+        nls.builtins.diagnostics.markdownlint,
+        nls.builtins.diagnostics.selene.with({
+          condition = function(utils)
+            return utils.root_has_file({ "selene.toml" })
+          end,
+        }),
+        nls.builtins.formatting.isort,
+        nls.builtins.formatting.black,
+        nls.builtins.diagnostics.flake8,
+      })
+    end,
+  },
 }
